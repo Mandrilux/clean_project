b33b55b46b457f0722d372b8a1b6a4f06fe05de1
epitech2020 <baptiste.heraud@epitech.eu>
2016-05-18 12:23:46 +0200

    init

---
+++ include/my.h
@@ -0,0 +1,17 @@
+/*
+** my.h for  in /home/baptiste/project/clean_project
+**
+** Made by
+** Login   <baptiste@epitech.net>
+**
+** Started on  Wed May 18 11:21:36 2016
+** Last update Wed May 18 11:22:34 2016 
+*/
+
+#ifndef MY_H_
+#define MY_H_
+		/* main.c */
+
+int		 main();
+
+#endif /* MY_H_ */
---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for  in /home/baptiste/project/clean_project
+**
+** Made by
+** Login   <baptiste@epitech.net>
+**
+** Started on  Wed May 18 11:20:55 2016
+** Last update Wed May 18 11:21:05 2016 
+*/
+
+int	main()
+{
+  return (0);
+}
